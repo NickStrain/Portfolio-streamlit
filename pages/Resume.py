d60de08fef197bf32b42acb985854a623b234659
Shugavaneshwar <shugavaneshwar13@gmail.com>
2024-09-26 13:26:39 +0530

    update

--- pages/Resume.py
+++ pages/Resume.py
@@ -1,13 +1,15 @@
 import streamlit as st
 
-# Function to display a PDF from a URL
-def display_pdf_from_url(pdf_url):
-    pdf_display = f'<iframe src="{pdf_url}" width="900" height="1000" type="application/pdf"></iframe>'
-    st.markdown(pdf_display, unsafe_allow_html=True)
-
 # Streamlit App
-st.title("Resume")
+st.title("Download Resume")
 
-# PDF hosted on a URL (replace with the actual URL of your PDF file)
-pdf_url = "img/redes/shugavaneshwar_resume.pdf"
-display_pdf_from_url(pdf_url)
+# PDF hosted URL (replace with your actual URL)
+pdf_url = "img/redes/shugavaneshwar_resume.pdf"  # Update with your PDF URL
+
+# Button to download the PDF
+st.download_button(
+    label="Download Resume",
+    data=pdf_url,
+    file_name="your_resume.pdf",  # Set the filename for the downloaded file
+    mime="application/pdf"  # Set the MIME type for PDF
+)

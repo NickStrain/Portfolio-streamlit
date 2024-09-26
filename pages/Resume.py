b9c4365e220307709348606c8df0ba6299745f74
Shugavaneshwar <shugavaneshwar13@gmail.com>
2024-09-26 13:28:27 +0530

    update

--- pages/Resume.py
+++ pages/Resume.py
@@ -1,15 +1,23 @@
 import streamlit as st
 
+# Function to read the PDF file
+def load_pdf(pdf_file_path):
+    with open(pdf_file_path, "rb") as f:
+        return f.read()
+
 # Streamlit App
 st.title("Download Resume")
 
-# PDF hosted URL (replace with your actual URL)
-pdf_url = "img/redes/shugavaneshwar_resume.pdf"  # Update with your PDF URL
+# Path to your PDF file (ensure it's in the correct location)
+pdf_file_path = "img/redes/shugavaneshwar_resume.pdf"  # Replace with your local PDF path
+
+# Load the PDF content
+pdf_data = load_pdf(pdf_file_path)
 
 # Button to download the PDF
 st.download_button(
     label="Download Resume",
-    data=pdf_url,
-    file_name="your_resume.pdf",  # Set the filename for the downloaded file
-    mime="application/pdf"  # Set the MIME type for PDF
+    data=pdf_data,
+    file_name="your_resume.pdf",
+    mime="application/pdf"
 )

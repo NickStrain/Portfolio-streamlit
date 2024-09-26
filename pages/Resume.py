e1dd18e8aa26e3982092b6638de8b51956af72c9
Shugavaneshwar <shugavaneshwar13@gmail.com>
2024-09-26 13:14:09 +0530

    update

--- pages/Resume.py
+++ pages/Resume.py
@@ -9,5 +9,5 @@ def display_pdf_from_url(pdf_url):
 st.title("Resume")
 
 # PDF hosted URL (replace with your actual URL)
-pdf_url = "https://drive.google.com/drive/u/0/my-drive"  # Update with your PDF URL
+pdf_url = "https://github.com/NickStrain/Portfolio-streamlit/blob/main/img/redes/shugavaneshwar_resume.pdf"  # Update with your PDF URL
 display_pdf_from_url(pdf_url)
